4106bf42ab9c717c2a8e2bdb54cd814c15afbb8e
Rahul Lal <Rahul21Lal@gmail.com>
2025-11-26 21:22:00 +1300

    Initial Commit

---
+++ HomePage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prototype
+{
+    /// <summary>
+    /// Interaction logic for EuropeanBurgers.xaml
+    /// </summary>
+    public partial class EuropeanBurgers : Page
+    {
+        public EuropeanBurgers()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSingleOlympian_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDoubleOlympian_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSingleParisian_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDoubleParisian_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSingleRoma_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDoubleRoma_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSingleMatador_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDoubleMatador_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSingleKaiser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDoubleKaiser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
