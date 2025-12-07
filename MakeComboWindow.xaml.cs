b8213cd1b07dcbf655b53988dc805ecab9769bb4
Rahul Lal <Rahul21Lal@gmail.com>
2025-12-07 22:17:25 +1300

    Setting up messagebox
    For making a video, a combo.

---
+++ MakeComboWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prototype
+{
+    /// <summary>
+    /// Interaction logic for MakeComboWindow.xaml
+    /// </summary>
+    public partial class MakeComboWindow : Window
+    {
+        public MakeComboWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLargeCombo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMediumCombo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnJustTheBurger_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
